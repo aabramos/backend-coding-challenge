c065a8162bd3abd28f47453d19efce629cf0cefe
aabramos <aalbertobramos@gmail.com>
2018-09-16 19:11:18 -0300

    Render a table below the field Minor adjustments

--- app/views.py
+++ app/views.py
@@ -2,13 +2,19 @@ from flask import render_template, make_response,\
     request, redirect, url_for
 from flask_restful import Resource
 from app.forms import UnbabelForm
+from app.models import Translation
 
 
 class Index(Resource):
     def get(self):
         headers = {'Content-Type': 'text/html'}
         form = UnbabelForm()
-        return make_response(render_template('index.html', title='Unbabel Coding Challenge', form=form), 200, headers)
+        translations = Translation.query.all()
+        return make_response(
+            render_template('index.html', title='Unbabel Coding Challenge', form=form, translations=translations),
+            200,
+            headers,
+        )
 
     def post(self):
         input_text = request.form.get('input_field')
