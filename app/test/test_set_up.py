5cdf6c7e77b6003807f712961da0dbcf9b86cf2e
aabramos <aalbertobramos@gmail.com>
2018-09-21 14:51:57 -0300

    Tests for UnbabelAPI

--- app/test/test_set_up.py
+++ app/test/test_set_up.py
@@ -10,6 +10,16 @@ from config import TestConfig
 
 
 class BasicTests(TestCase):
+    '''
+        This will test the basic setup. After running this test,
+        make sure to run:
+
+            flask db migrate
+            flask db upgrade
+
+        To create and apply the database migrations to run the
+        other tests.
+    '''
     SQLALCHEMY_DATABASE_URI = TestConfig.SQLALCHEMY_DATABASE_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = TestConfig.SQLALCHEMY_TRACK_MODIFICATIONS
     TESTING = TestConfig.DEBUG
--- app/test/test_unbabel.py
+++ app/test/test_unbabel.py
@@ -1,2 +1,49 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import unittest
+from app import create_app
+from flask_testing import TestCase
+from unbabel.api import UnbabelApi
+from config import TestConfig
+
+
+class BasicTests(TestCase):
+    SQLALCHEMY_DATABASE_URI = TestConfig.SQLALCHEMY_DATABASE_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = TestConfig.SQLALCHEMY_TRACK_MODIFICATIONS
+    TESTING = TestConfig.DEBUG
+    DEBUG = TestConfig.DEBUG
+
+    def create_app(self):
+        return create_app(self)
+
+    def test_unbabelAPI(self):
+        api = UnbabelApi(
+            username=TestConfig.UNBABEL_SANDBOX_USERNAME,
+            api_key=TestConfig.UNBABEL_SANDBOX_KEY,
+            sandbox=True,
+        )
+        response = api.post_translations(
+            text='Test translation',
+            target_language='es',
+            source_language='en',
+        )
+        self.assertEqual(response.status, 'new')
+        self.assertIsNone(response.translation)
+        self.assertTrue(response.uid)
+        self.assertEqual(response.target_language, 'es')
+        self.assertEqual(response.source_language, 'en')
+        self.assertIsNone(response.client)
+        self.assertIsNone(response.balance)
+        self.assertIsNone(response.order_number)
+        self.assertIsNone(response.origin)
+        self.assertTrue(response.price)
+        self.assertEqual(response.text_format, 'text')
+        self.assertIsNone(response.price_plan)
+        self.assertIsNone(response.topics)
+        self.assertEqual(response.translators, [])
+        self.assertIn('Test translation', response.text)
+
+
+if __name__ == '__main__':
+    unittest.main()
